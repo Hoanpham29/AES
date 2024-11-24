bf5bee71855b0dfe50714379c2a80a04690a1d96
HoanPham29 <hoanpham2911@gmail.com>
2024-11-24 19:38:28 +0700

    Add project files.

---
+++ AES/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace AES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rb256 = new System.Windows.Forms.RadioButton();
+            this.rb192 = new System.Windows.Forms.RadioButton();
+            this.rb128 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCiphertext = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.txtPlaintext = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.txtPlainCiphertext = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPlainCiphertext);
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnDecrypt);
+            this.groupBox1.Controls.Add(this.btnEncrypt);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.rb256);
+            this.groupBox1.Controls.Add(this.rb192);
+            this.groupBox1.Controls.Add(this.rb128);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtCiphertext);
+            this.groupBox1.Controls.Add(this.txtKey);
+            this.groupBox1.Controls.Add(this.txtPlaintext);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(423, 554);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(240, 498);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(102, 34);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(75, 498);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(102, 34);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Xóa";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(240, 448);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(102, 34);
+            this.btnDecrypt.TabIndex = 10;
+            this.btnDecrypt.Text = "Giải mã";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(75, 448);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(102, 34);
+            this.btnEncrypt.TabIndex = 9;
+            this.btnEncrypt.Text = "Mã hóa";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Bản mã";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Khóa";
+            // 
+            // rb256
+            // 
+            this.rb256.AutoSize = true;
+            this.rb256.Location = new System.Drawing.Point(293, 133);
+            this.rb256.Name = "rb256";
+            this.rb256.Size = new System.Drawing.Size(111, 22);
+            this.rb256.TabIndex = 6;
+            this.rb256.TabStop = true;
+            this.rb256.Text = "Khóa 256 bit";
+            this.rb256.UseVisualStyleBackColor = true;
+            // 
+            // rb192
+            // 
+            this.rb192.AutoSize = true;
+            this.rb192.Location = new System.Drawing.Point(154, 133);
+            this.rb192.Name = "rb192";
+            this.rb192.Size = new System.Drawing.Size(111, 22);
+            this.rb192.TabIndex = 5;
+            this.rb192.TabStop = true;
+            this.rb192.Text = "Khóa 192 bit";
+            this.rb192.UseVisualStyleBackColor = true;
+            // 
+            // rb128
+            // 
+            this.rb128.AutoSize = true;
+            this.rb128.Location = new System.Drawing.Point(15, 133);
+            this.rb128.Name = "rb128";
+            this.rb128.Size = new System.Drawing.Size(111, 22);
+            this.rb128.TabIndex = 4;
+            this.rb128.TabStop = true;
+            this.rb128.Text = "Khóa 128 bit";
+            this.rb128.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Bản rõ";
+            // 
+            // txtCiphertext
+            // 
+            this.txtCiphertext.Location = new System.Drawing.Point(15, 291);
+            this.txtCiphertext.Multiline = true;
+            this.txtCiphertext.Name = "txtCiphertext";
+            this.txtCiphertext.Size = new System.Drawing.Size(389, 67);
+            this.txtCiphertext.TabIndex = 2;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(15, 187);
+            this.txtKey.Multiline = true;
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(389, 67);
+            this.txtKey.TabIndex = 1;
+            // 
+            // txtPlaintext
+            // 
+            this.txtPlaintext.Location = new System.Drawing.Point(15, 46);
+            this.txtPlaintext.Multiline = true;
+            this.txtPlaintext.Name = "txtPlaintext";
+            this.txtPlaintext.Size = new System.Drawing.Size(389, 67);
+            this.txtPlaintext.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtOutput);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(441, 23);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(647, 554);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(6, 27);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(635, 521);
+            this.txtOutput.TabIndex = 0;
+            this.txtOutput.Text = "";
+            // 
+            // txtPlainCiphertext
+            // 
+            this.txtPlainCiphertext.Location = new System.Drawing.Point(15, 375);
+            this.txtPlainCiphertext.Multiline = true;
+            this.txtPlainCiphertext.Name = "txtPlainCiphertext";
+            this.txtPlainCiphertext.Size = new System.Drawing.Size(389, 48);
+            this.txtPlainCiphertext.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1100, 589);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AES";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rb256;
+        private System.Windows.Forms.RadioButton rb192;
+        private System.Windows.Forms.RadioButton rb128;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCiphertext;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.TextBox txtPlaintext;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.TextBox txtPlainCiphertext;
+    }
+}
+
